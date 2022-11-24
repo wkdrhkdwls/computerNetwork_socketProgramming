004e3f43a22a401b51114bc945ac7702b6e96514
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2022-11-24 16:53:08 +0900

    ComputerNetwork-최종
    socket통신 - 버스예약서비스

--- my_Client.py
+++ my_Client.py
@@ -2,12 +2,11 @@ from tkinter import *
 import socket
 from threading import Thread
     
-IP = ""
-PORT = 0
+IP = "192.168.55.67"
+PORT = 12000
 
 resvSeats = []
 seatButtons = []
-resvRadioButtons = []
 
 seat_list = [
     '1A','1B','','1C', #0
@@ -28,33 +27,45 @@ def connect(event=None):
     PORT = int(addr[1])
     w_connect.destroy()           
 
-def recv_reservation(sock):
+def recv_msg(sock):
     while True:
-        msg = sock.recv(1024)
-        print(msg.decode())
-        i = int(msg.decode())
+        data = sock.recv(1024)
+        msg = data.decode()
+        i = int(msg[1:])
+        if msg[:1] == "0":
+            seatButtons[i].configure(bg = "blue")
+            if seatButtons[i] not in resvSeats:
+                seatButtons[i].configure(state=DISABLED)
+        elif msg[:1] == "1":
+            seatButtons[i].configure(bg = "SystemButtonFace")
+            seatButtons[i].configure(state=NORMAL)
+        
+        
+def send_msg(i):
+    if seatButtons[i]['bg'] == "blue":
+        #cancel
+        msg = "1"+str(i)
+        sock.send(msg.encode())
+        seatButtons[i].configure(bg = "SystemButtonFace")
+        resvBox.insert(END, '{}번 좌석 취소'.format(seat_list[i]))
+        resvBox.see(END)
+        resvSeats.remove(seatButtons[i])
+    else:
+        #reservation
+        msg = "0"+str(i)
+        sock.send(msg.encode())
         seatButtons[i].configure(bg = "blue")
+        resvBox.insert(END, '{}번 좌석 예약'.format(seat_list[i]))
+        resvBox.see(END)
 
-
-def send_reservation(i):
-    msg = str(i)
-    sock.send(msg.encode())
-    
-    seatButtons[i].configure(bg = "blue")
-    seatButtons[i].configure(state=DISABLED)
-    label1 = Label(rsvFrame,text='{}번 좌석 예약'.format(seat_list[i]),fg="blue")
-    label1.pack(side = TOP)
     #이 리스트는 내 클라이언트에서 예약된 좌석을 의미
-    resvSeats.append(seatButtons[i])
-
-
-
+        resvSeats.append(seatButtons[i])
 
 #서버 연결 부분
 w_connect = Tk()
 w_connect.title("접속 대상")
 Label(w_connect, text="접속 대상").grid(row=0, column=0)
-input_string = StringVar(value="127.0.0.1:12000")
+input_string = StringVar(value="192.168.55.67:12000")
 input_addr = Entry(w_connect, textvariable=input_string, width=20)
 input_addr.grid(row=0, column=1, padx=5, pady=5)
 c_button = Button(w_connect, text="접속하기", command=connect)
@@ -75,8 +86,8 @@ w_connect.mainloop()
 #예약부분
 window = Tk()
 
-window_width = 720
-window_height = 800
+window_width = 600
+window_height = 750
 window_x = 700
 window_y = 100
 
@@ -86,11 +97,14 @@ window.title("버스 좌석 예매")
 
 seatFrame = Frame(window, width = 0, height = 0, relief = "sunken", bd = 1, bg = "white")
 rsvFrame = Frame(window, width = 0, height = 0, relief = "sunken", bd = 1)
-# seatFrame.grid(column=0, row=0, padx = 10, pady = 25)
-# rsvFrame.grid(column=1, row=0, padx = 10, pady = 25)
-seatFrame.pack(side="left",fill="both",expand=True)
-rsvFrame.pack(side="right",fill="both",expand=True)
+seatFrame.grid(column=0, row=0, padx = 10, pady = 25)
+rsvFrame.grid(column=1, row=0, padx = 10, pady = 25)
 
+scroll = Scrollbar(rsvFrame)
+resvBox = Listbox(rsvFrame, height=20, width=20, yscrollcommand=scroll.set)
+
+resvBox.pack(side=LEFT, fill=BOTH, padx=5, pady=5)
+scroll.pack(side=RIGHT, fill=Y)
 
 seat_col = 0
 seat_row = 0
@@ -98,7 +112,7 @@ seat_row = 0
 #좌석 배치
 for i in range(len(seat_list)):
     def action(t = i):
-        send_reservation(t)
+        send_msg(t)
         
     if seat_row == 8:
         #중간에 레이블 없이
@@ -119,17 +133,11 @@ for i in range(len(seat_list)):
             seat_col = 0
     seatButtons.append(seat_button)
 
-
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((IP,PORT))
 
-th1 = Thread(target=recv_reservation, args=(sock, ))
+th1 = Thread(target=recv_msg, args=(sock, ))
 th1.daemon = True
 th1.start()
-
-#th2 = Thread(target=recv_permission, args=(sock, ))
-#th2.daemon = True
-#th2.start()
             
 window.mainloop()
--- my_Server.py
+++ my_Server.py
@@ -6,28 +6,39 @@ class MyHandler(socketserver.BaseRequestHandler):
     users = {}
     resvList = []
     
-    def send_all_reserves(self, seat):
+    def send_all_msg(self, seat):
         for sock, _ in self.users.values():
             sock.send(seat.encode())
     
     def handle(self):
         print(self.client_address)
         self.users[self.client_address[1]] = (self.request, self.client_address)
+
         if len(self.resvList) !=0:
             sock, _ = self.users[self.client_address[1]]
             for i in self.resvList:
-                sock.send(i.encode())
-                sleep(0.01)
+                msg = "0"+str(i)
+                sock.send(msg.encode())
+                sleep(0.03)
               
         while True:
-            msg = self.request.recv(1024)
-            self.send_all_reserves(msg.decode())
-            self.resvList.append(msg.decode())
+            data = self.request.recv(1024)
+            msg = data.decode()
+
+            if msg[:1] == "0":   #header
+                self.resvList.append(msg[1:]) #index
+            elif msg[:1] == "1":
+                self.resvList.remove(msg[1:])  
+            self.send_all_msg(msg)
+            print(self.resvList)           
             
 class ChatServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
-server = ChatServer(("",12000), MyHandler)
+IP = "192.168.55.67"
+PORT = 12000
+
+server = ChatServer((IP,PORT), MyHandler)
 server.serve_forever()
 server.shutdown()
 server.server_close()
